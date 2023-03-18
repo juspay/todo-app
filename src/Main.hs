0eec2f79710b2a6442d55f91974479560f36944e
sbh69840 <sbh69840@gmail.com>
2023-03-19 04:00:11 +0530

    TodoApp/Request: Completed implementation of all functions markes undefined

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,21 @@
 
 module Main (main) where
 
-import TodoApp.Request ()
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
+import Text.Read (readMaybe)
+import TodoApp.Request (add, delete, done, reset, view)
 
 main :: IO ()
-main = print "Hello World!"
+main = do
+  domain <- fromMaybe "http://localhost" <$> lookupEnv "TODO_DOMAIN"
+  todoPort <- fromMaybe 3000 . (readMaybe =<<) <$> lookupEnv "TODO_PORT"
+  -- Test
+  view (domain, todoPort)
+  reset (domain, todoPort)
+  add "do bad things" (domain, todoPort)
+  done 1 (domain, todoPort)
+  delete 1 (domain, todoPort)
+  view (domain, todoPort)
+
+-- TODO: Use boxes to display the output of the view function
--- src/TodoApp/Request.hs
+++ src/TodoApp/Request.hs
@@ -5,71 +5,81 @@
 
 module TodoApp.Request (done, view, add, delete, reset) where
 
--- TODO: maybe use qualified import?
-
 import Control.Monad.IO.Class (MonadIO)
-import Data.Aeson (ToJSON, Value)
+import Data.Aeson (object, (.=))
+import Data.ByteString (ByteString)
 import Data.Kind (Type)
 import Data.Maybe (fromJust)
-import Data.Text (concat, pack)
-import GHC.IO (liftIO)
-import Network.HTTP.Req
-  ( AllowsBody,
-    CanHaveBody (CanHaveBody),
-    HttpBodyAllowed,
-    HttpMethod,
-    ReqBodyJson (..),
-    defaultHttpConfig,
-    ignoreResponse,
-    jsonResponse,
-    port,
-    req,
-    responseBody,
-    responseStatusCode,
-    runReq,
-    useHttpURI,
-  )
-import Relude (fromMaybe, lookupEnv, readMaybe, toText)
+import Data.Text (Text, concat, pack)
+import qualified Network.HTTP.Req as R
 import Text.URI (mkURI)
 import Prelude hiding (concat)
 
 data Response = Response
-  { message :: Value,
+  { message :: ByteString,
     responseCode :: Int
   }
-
+-- TODO: Add more comments
+-- |Make a http request to postgrest service
 request ::
-  forall {method} {m :: Type -> Type} {a}.
-  ( HttpBodyAllowed (AllowsBody method) 'CanHaveBody,
+  forall {method} {m :: Type -> Type} {body} {response}.
+  ( R.HttpBodyAllowed (R.AllowsBody method) (R.ProvidesBody body),
     MonadIO m,
-    HttpMethod method,
-    ToJSON a
+    R.HttpMethod method,
+    R.HttpBody body
   ) =>
-  a ->
+  body ->
   method ->
+  Text ->
+  Text ->
+  (String, Int) ->
   m Response
-request payload method =
-  runReq defaultHttpConfig $ do
-    -- TODO: Move domain and port to main and use IORef to Store the state
-    domain <- fromMaybe "http://localhost" <$> lookupEnv "TODO_DOMAIN"
-    todoPort <- (fromMaybe 3000 <$> readMaybe) . fromMaybe "3000" <$> lookupEnv "TODO_PORT"
-    uri <- mkURI $ concat [toText domain, "/todos"]
-    let (url, _) = fromJust (useHttpURI uri)
+request body method subdir filter (domain, todoPort) =
+  R.runReq R.defaultHttpConfig $ do
+    uri <- mkURI $ concat [pack domain, subdir, "?", filter]
+    let (url, options) = fromJust (R.useHttpURI uri)
     r <-
-      req
+      R.req
         method
         url
-        (ReqBodyJson payload) -- use built-in options or add your own
-        jsonResponse
-        (port todoPort) -- query params, headers, explicit port number, etc.
-    return Response {message = responseBody r, responseCode = responseStatusCode r :: Int}
+        body
+        R.bsResponse
+        $ R.port todoPort <> options -- options include the query parameters that help in filtering rows of a table
+    return Response {message = R.responseBody r, responseCode = R.responseStatusCode r :: Int}
 
-done = undefined
+-- |Mark the item with id as done
+done :: Int -> (String, Int) -> IO ()
+done id host = do
+  let payload =
+        object ["done" .= ("false" :: String)]
+  res <- request (R.ReqBodyJson payload) R.PATCH "/todos" (concat ["id=eq.", pack $ show id]) host
+  print $ responseCode res
 
-view = undefined
+-- TODO: add another function that only displays pending tasks
+-- |Display all the items in the table
+view :: (String, Int) -> IO ()
+view host = do
+  res <- request R.NoReqBody R.GET "/todos" "" host
+  print $ message res
 
-add = undefined
+-- |Add a new task to the table
+add :: String -> (String, Int) -> IO ()
+add task host = do
+  let payload =
+        object ["task" .= task]
+  res <- request (R.ReqBodyJson payload) R.POST "/todos" "" host
+  print $ responseCode res
 
-delete = undefined
+-- |Delete a TODO item with given id
+delete :: Int -> (String, Int) -> IO ()
+delete id host = do
+  res <- request R.NoReqBody R.DELETE "/todos" (concat ["id=eq.", pack $ show id]) host
+  print $ responseCode res
 
-reset = undefined
+-- |Remove all the TODO items from the table
+reset :: (String, Int) -> IO ()
+reset host = do
+  _ <- request R.NoReqBody R.DELETE "/todos" "" host
+  -- Call a SQL function that sets the sequence to start from 1
+  res <- request R.NoReqBody R.POST "/rpc/reset_id" "" host
+  print $ responseCode res
