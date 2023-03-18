848fdd76aa50ba8d523c71e52626a77f5c9df609
sbh69840 <sbh69840@gmail.com>
2023-03-18 22:58:59 +0530

    ormolu, cabal-fmt formatting and README updates

--- README.md
+++ README.md
@@ -1,33 +1,20 @@
-# haskell-template
+# haskell-flake-hello
 
-Get a Haskell development environment up and running quickly. Thanks to Nix, this template is optimized for a fully reproducible and friendly development environment. It is based on:
+Refer to [haskell-flake-hello.cabal](haskell-flake-hello.cabal) for description.
 
-- [Nix](https://srid.ca/haskell-nix) + [Flakes](https://serokell.io/blog/practical-nix-flakes) (via [`github:srid/haskell-flake`](https://github.com/srid/haskell-flake)) + GHC 9.2
-- VSCode + [HLS](https://github.com/haskell/haskell-language-server)
-- [fourmolu](https://github.com/fourmolu/fourmolu) autoformatting 
-- [Relude](https://github.com/kowainik/relude#relude) as Prelude.
-  - `.hlint.yaml` is [from relude](https://github.com/kowainik/relude/blob/main/.hlint.yaml)
-- Devshell scripts (`,` prefixed) are provided via [mission-control](https://github.com/Platonic-Systems/mission-control)
+## Prerequisite
 
-If you have an *existing* Haskell project, you should probably use https://github.com/srid/haskell-flake instead.
+- Install Nix<sup>[(*)](https://determinate.systems/posts/determinate-nix-installer)</sup>, enable Flakes (avoid using global installations)
+
+TODO: Add instructions to run postgres in the local directory
+TODO: Add instructions to start postgrest along with creating the schema.
 
 ## Getting Started
 
-*tldr: Install Nix, enable Flakes, open in VSCode and run `, run`.*
-
-For details, see: https://srid.ca/haskell-template/start
+TODO: Write about how to use the app
 
 ## Tips
 
-- Run `nix flake update` to update all flake inputs.
-- Run `nix build .#check -L` to run the flake checks.
-- Run `, fmt` in nix shell to autoformat the project. This uses [treefmt](https://github.com/numtide/treefmt).
-- Run `, docs` to start Hoogle with packages in your cabal file.
-- Run the application without installing: `nix run github:srid/haskell-template` (or `nix run .` from checkout)
-- Common workflows
-  - Adding library dependencies in Nix: https://haskell.flake.page/dependency
-  - Adding tests: https://srid.ca/haskell-template/tests
+- Run `nix run nixpkgs#ormolu -- --mode inplace $(git ls-files '*.hs')` to format all the `.hs` files in your worktree.
+- Run `nix run nixpkgs#haskellPackages.cabal-fmt -- --inplace haskell-flake-hello.cabal` to format the `.cabal` file.
 
-## Discussions
-
-Questions? Ideas? Suggestions? You may post them here: https://github.com/srid/haskell-template/discussions
--- src/TodoApp/Request.hs
+++ src/TodoApp/Request.hs
@@ -1,45 +1,58 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DataKinds #-}
 
 module TodoApp.Request (done, view, add, delete, reset) where
+
 -- TODO: maybe use qualified import?
-import Network.HTTP.Req
-    ( responseStatusCode,
-      port,
-      ignoreResponse,
-      req,
-      useHttpURI,
-      defaultHttpConfig,
-      runReq,
-      ReqBodyJson(..),
-      HttpMethod, jsonResponse, responseBody, HttpBodyAllowed, AllowsBody, CanHaveBody (CanHaveBody))
-import Text.URI (mkURI)
+
+import Control.Monad.IO.Class (MonadIO)
+import Data.Aeson (ToJSON, Value)
+import Data.Kind (Type)
 import Data.Maybe (fromJust)
 import Data.Text (concat, pack)
-import Prelude hiding (concat)
-import Data.Aeson (Value, ToJSON)
-import Data.Kind (Type)
-import Control.Monad.IO.Class (MonadIO)
 import GHC.IO (liftIO)
-import Relude (lookupEnv, toText, fromMaybe, readMaybe)
+import Network.HTTP.Req
+  ( AllowsBody,
+    CanHaveBody (CanHaveBody),
+    HttpBodyAllowed,
+    HttpMethod,
+    ReqBodyJson (..),
+    defaultHttpConfig,
+    ignoreResponse,
+    jsonResponse,
+    port,
+    req,
+    responseBody,
+    responseStatusCode,
+    runReq,
+    useHttpURI,
+  )
+import Relude (fromMaybe, lookupEnv, readMaybe, toText)
+import Text.URI (mkURI)
+import Prelude hiding (concat)
 
-data Response = Response {
-   message :: Value
-  ,responseCode :: Int
-}
-request :: forall {method} {m :: Type -> Type} {a}.
-  (HttpBodyAllowed (AllowsBody method) 'CanHaveBody,
-   MonadIO m,
-   HttpMethod method,
-   ToJSON a) =>
-   a -> method -> m Response
-request payload method = 
+data Response = Response
+  { message :: Value,
+    responseCode :: Int
+  }
+
+request ::
+  forall {method} {m :: Type -> Type} {a}.
+  ( HttpBodyAllowed (AllowsBody method) 'CanHaveBody,
+    MonadIO m,
+    HttpMethod method,
+    ToJSON a
+  ) =>
+  a ->
+  method ->
+  m Response
+request payload method =
   runReq defaultHttpConfig $ do
-    -- TODO: Move domain and port to main and use IORef to Store the state 
+    -- TODO: Move domain and port to main and use IORef to Store the state
     domain <- fromMaybe "http://localhost" <$> lookupEnv "TODO_DOMAIN"
-    todoPort <- ( fromMaybe 3000 <$> readMaybe ) . fromMaybe "3000" <$> lookupEnv "TODO_PORT"
+    todoPort <- (fromMaybe 3000 <$> readMaybe) . fromMaybe "3000" <$> lookupEnv "TODO_PORT"
     uri <- mkURI $ concat [toText domain, "/todos"]
     let (url, _) = fromJust (useHttpURI uri)
     r <-
@@ -49,7 +62,7 @@ request payload method =
         (ReqBodyJson payload) -- use built-in options or add your own
         jsonResponse
         (port todoPort) -- query params, headers, explicit port number, etc.
-    return Response { message = responseBody r ,responseCode = responseStatusCode r :: Int }
+    return Response {message = responseBody r, responseCode = responseStatusCode r :: Int}
 
 done = undefined
 
@@ -60,4 +73,3 @@ add = undefined
 delete = undefined
 
 reset = undefined
-
