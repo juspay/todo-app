c0a5133b213734e43c6c29bb071ebcbafcd14250
sbh69840 <sbh69840@gmail.com>
2023-03-21 14:56:23 +0530

    Pretty print using boxes
    Parsing arguments to use the TODO app
    
    Request.hs: refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,141 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main (main) where
 
-import qualified Main.Utf8 as Utf8
+import Data.Aeson (Result (..))
 import Data.Maybe (fromMaybe)
+import Data.Semigroup ((<>))
+import qualified Data.Text as DT
+import qualified Main.Utf8 as Utf8
+import Options.Applicative hiding (Success)
 import System.Environment (lookupEnv)
+import Text.PrettyPrint.Boxes hiding ((<>))
 import Text.Read (readMaybe)
-import TodoApp.Request (add, delete, complete, reset, view, viewPending)
-import Text.PrettyPrint.Boxes
-    ( (//), (<+>), para, printBox, right, text )
-import Data.Aeson (Result(..))
+import TodoApp.Request (Task)
+import qualified TodoApp.Request as TR
+
+newtype Opts = Opts {optCommand :: Command}
+
+data Command
+  = Add String
+  | Delete Int
+  | Done Int
+  | View
+  | ViewAll
+  | Reset
 
 main :: IO ()
 main = do
   domain <- fromMaybe "http://localhost" <$> lookupEnv "TODO_DOMAIN"
   todoPort <- fromMaybe 3000 . (readMaybe =<<) <$> lookupEnv "TODO_PORT"
-  -- Test
-  view (domain, todoPort)
-  reset (domain, todoPort)
-  add "do bad things" (domain, todoPort)
-  add "do bad things" (domain, todoPort)
-  complete 1 (domain, todoPort)
-  delete 1 (domain, todoPort)
-  res <- view (domain, todoPort)
-  case res of
-    Success a -> print a
-    Error b -> print b
-  print <$> viewPending (domain, todoPort)
-  Utf8.withUtf8 $ do
-    printBox (( text "✗" <+> para right 100 "Hello 🌎" ) // ( text "✓" <+> para right 100 "Hello 🌎"))
+  (opts :: Opts) <- execParser optsParser
+  case optCommand opts of
+    Add task -> do
+      status <- TR.add task (domain, todoPort)
+      parseStatus status "Task added!"
+    Delete id -> do
+      status <- TR.delete id (domain, todoPort)
+      parseStatus status "Task deleted!"
+    Done id -> do
+      status <- TR.complete id (domain, todoPort)
+      parseStatus status "Task completed!"
+    View -> do
+      todo <- TR.view (domain, todoPort)
+      printWithUtf8 todo
+    ViewAll -> do
+      todo <- TR.viewAll (domain, todoPort)
+      printWithUtf8 todo
+    Reset -> do
+      status <- TR.reset (domain, todoPort)
+      parseStatus status "Tasks cleared!"
+  where
+    printWithUtf8 :: Result [Task] -> IO ()
+    printWithUtf8 res = do
+      case res of
+        Success a -> mapM_ printTask a
+        Error b -> putStrLn b
+    parseStatus :: Int -> String -> IO ()
+    parseStatus status message = do
+      let isStatusSuccess = status > 200 && status <= 299
+          error = "Something went wrong!"
+      if isStatusSuccess then putStrLn message else putStrLn error
 
--- TODO: Use boxes to display the output of the view function
+    optsParser :: ParserInfo Opts
+    optsParser =
+      info
+        (helper <*> versionOption <*> programOptions)
+        ( fullDesc
+            <> progDesc "optparse subcommands example"
+            <> header
+              "optparse-sub-example - a small example program for optparse-applicative with subcommands"
+        )
+    versionOption :: Parser (a -> a)
+    versionOption = infoOption "0.0" (long "version" <> help "Show version")
+    programOptions :: Parser Opts
+    programOptions =
+      Opts <$> hsubparser (addCommand <> deleteCommand <> doneCommand <> viewCommand <> viewAllCommand <> resetCommand)
+    addCommand :: Mod CommandFields Command
+    addCommand =
+      command
+        "add"
+        (info addOptions (progDesc "Add a task"))
+    addOptions :: Parser Command
+    addOptions =
+      Add
+        <$> strArgument (metavar "Task" <> help "Task to add in TODO")
+    deleteCommand :: Mod CommandFields Command
+    deleteCommand =
+      command
+        "delete"
+        (info addOptions (progDesc "Delete a task"))
+    deleteOptions :: Parser Command
+    deleteOptions =
+      Delete
+        <$> option auto (metavar "Task ID" <> help "ID of the task to delete from TODO")
+    doneCommand :: Mod CommandFields Command
+    doneCommand =
+      command
+        "done"
+        (info addOptions (progDesc "Mark a task as complete"))
+    doneOptions :: Parser Command
+    doneOptions =
+      Done
+        <$> option auto (metavar "Task ID" <> help "ID of the task to mark as complete")
+    viewCommand :: Mod CommandFields Command
+    viewCommand =
+      command
+        "view"
+        (info (pure View) (progDesc "View list of pending TODOs"))
+    -- TODO: make this an extension for view command, for example: view -a should print
+    -- all tasks
+    viewAllCommand :: Mod CommandFields Command
+    viewAllCommand =
+      command
+        "viewAll"
+        (info (pure ViewAll) (progDesc "View list of all TODOs"))
+    resetCommand :: Mod CommandFields Command
+    resetCommand =
+      command
+        "reset"
+        (info (pure Reset) (progDesc "Clear the TODO list"))
+    -- \|Set the width of the box that displays the list of TODO's
+    width :: Int
+    width = 50
+
+    getCheckBox :: Bool -> String
+    getCheckBox True = "✗ "
+    getCheckBox False = "✓ "
+
+    printTask :: TR.Task -> IO ()
+    printTask v = do
+      printBox
+        -- Move the task row to the right by 2 spaces
+        ( moveRight
+            2
+            -- Prints `✓ <id>`
+            (text (getCheckBox (TR.done v) ++ show (TR.id v)))
+            <+>
+            -- Prints task in a box whose width is `width` and
+            -- height depends on the length of the taks message
+            para left width (DT.unpack $ TR.task v)
+        )
+      -- Print an extra line to separate two tasks
+      printBox $ text " "
--- src/TodoApp/Request.hs
+++ src/TodoApp/Request.hs
@@ -1,38 +1,33 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-
-module TodoApp.Request (complete, view, add, delete, reset, viewPending) where
+module TodoApp.Request (complete, viewAll, add, delete, reset, view, Task (..)) where
 
 import Control.Monad.IO.Class (MonadIO)
-import Data.Aeson (object, (.=), decode, fromJSON, Result)
+import Data.Aeson (Result, decode, fromJSON, object, (.=))
+import Data.Aeson.Types (FromJSON)
 import Data.ByteString.Lazy.Internal (ByteString)
 import Data.Kind (Type)
 import Data.Maybe (fromJust, fromMaybe)
 import Data.Text (Text, concat, pack)
+import GHC.Generics (Generic)
 import qualified Network.HTTP.Req as R
 import Text.URI (mkURI)
 import Prelude hiding (concat)
-import Data.Aeson.TH (defaultOptions, deriveFromJSON)
 
 data Response = Response
   { message :: ByteString,
     responseCode :: Int
   }
 
-data View = View 
-  {
-    id :: Int,
+data Task = Task
+  { id :: Int,
     done :: Bool,
     task :: Text
   }
-  deriving (Show, Eq)
--- Using TemplateHaskell to generate FromJSON instance for View
-$(deriveFromJSON defaultOptions ''View)
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (FromJSON)
 
 -- TODO: Add more comments
--- |Make a http request to postgrest service
+
+-- | Make a http request to postgrest service
 request ::
   ( R.HttpBodyAllowed (R.AllowsBody method) (R.ProvidesBody body),
     MonadIO m,
@@ -58,7 +53,7 @@ request body method subdir filter (domain, todoPort) =
         $ R.port todoPort <> options -- options include the query parameters that help in filtering rows of a table
     return Response {message = R.responseBody r, responseCode = R.responseStatusCode r :: Int}
 
--- |Mark the item with id as done
+-- | Mark the item with id as done
 complete :: Int -> (String, Int) -> IO Int
 complete id host = do
   let payload =
@@ -66,19 +61,19 @@ complete id host = do
   res <- request (R.ReqBodyJson payload) R.PATCH "/todos" (concat ["id=eq.", pack $ show id]) host
   return $ responseCode res
 
--- |Return all the items in the table
-view :: (String, Int) -> IO (Result [View])
-view host = do
+-- | Return all the items in the table
+viewAll :: (String, Int) -> IO (Result [Task])
+viewAll host = do
   res <- request R.NoReqBody R.GET "/todos" "" host
   return $ fromJSON $ fromMaybe (object []) $ decode $ message res
 
--- |Return pending items in the table
-viewPending :: (String, Int) -> IO (Result [View])
-viewPending host = do
+-- | Return pending items in the table
+view :: (String, Int) -> IO (Result [Task])
+view host = do
   res <- request R.NoReqBody R.GET "/todos" "done=is.false" host
   return $ fromJSON $ fromMaybe (object []) $ decode $ message res
 
--- |Add a new task to the table
+-- | Add a new task to the table
 add :: String -> (String, Int) -> IO Int
 add task host = do
   let payload =
@@ -86,13 +81,13 @@ add task host = do
   res <- request (R.ReqBodyJson payload) R.POST "/todos" "" host
   return $ responseCode res
 
--- |Delete a TODO item with given id
+-- | Delete a TODO item with given id
 delete :: Int -> (String, Int) -> IO Int
 delete id host = do
   res <- request R.NoReqBody R.DELETE "/todos" (concat ["id=eq.", pack $ show id]) host
   return $ responseCode res
 
--- |Remove all the TODO items from the table
+-- | Remove all the TODO items from the table
 reset :: (String, Int) -> IO Int
 reset host = do
   _ <- request R.NoReqBody R.DELETE "/todos" "" host
