409db60684a3b25dc7c722b7cf74791e47acc0a5
sbh69840 <sbh69840@gmail.com>
2023-03-30 07:45:22 +0530

    tutorial init dump, flake comment fixes

--- flake.nix
+++ flake.nix
@@ -5,23 +5,22 @@
     let
       # The platforms which can use the flake
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      # Filter out supportedSystems from all the system attributes 
-      # available in nixpkgs
+      # A function that takes an attrset and returns the values
+      # for corresponding keys in supportedSystems (in other words, filter)
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       # Select the legacyPackages attribute for the supportedSystems
       # Note: legacyPackages is just a placeholder for all the packages
       #       in the package manager because naming it as packages will
       #       force the evaluation of all of them (80000+) during `nix flake show`
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-      # Overrides for the haskellPackages. It will replace the current
-      # version of the packages in haskellPackages with the one mentioned
-      # below.
-      overlay = self: super: {
+      # Define a function that takes two arguments and return a attrset
+      # of overrides to nixpkgs. Read more about Overlays here: https://nixos.wiki/wiki/Overlays
+      overlay = final: prev: {
         # `nix flake show` and `nix flake check` won't work because of
         # IFD (https://nixos.wiki/wiki/Import_From_Derivation).
         # Might be possible to use in the near future, check this out:
         # https://github.com/cdepillabout/cabal2nixWithoutIFD
-        todo-app = self.callCabal2nix "todo-app" ./. { };
+        todo-app = final.callCabal2nix "todo-app" ./. { };
       };
       # Extend the `pkgs.haskellPackages` attrset using an overlay.
       haskellPackages' = forAllSystems (system: pkgs.${system}.haskellPackages.extend overlay);
