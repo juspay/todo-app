f36d2015802d5f3e55a1dee73982131efea98e82
shivaraj-bh <sbh69840@gmail.com>
2023-10-17 16:40:46 +0530

    description for mkFlake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   };
 
   outputs = inputs:
+    # mkFlake takes two inputs, `args` and `mod`. Internally it calls `lib.evalModules`(https://nixos.org/manual/nixpkgs/unstable/#module-system-lib-evalModules)
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       perSystem = { self', pkgs, ... }:
