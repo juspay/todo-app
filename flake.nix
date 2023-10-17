ae937c6926a5b2b22447ceb3a230e5f3fe83c2ff
shivaraj-bh <sbh69840@gmail.com>
2023-10-17 15:16:54 +0530

    concise inputs arg to outputs

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-parts }:
-    flake-parts.lib.mkFlake { inputs = { inherit self nixpkgs flake-parts; }; } {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       perSystem = { self', pkgs, ... }:
         let
