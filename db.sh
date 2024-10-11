24265f372afcbd953b46555832f947f43185892c
shivaraj-bh <me@shivaraj-bh.in>
2024-10-11 15:21:42 +0530

    Remove `db.sh`
    This is taken care off by `createdb` flake app

--- db.sh
+++
@@ -1,8 +0,0 @@
-# Create configuration file for postgrest
-mkdir data
-echo "db-uri = \"postgres://authenticator:mysecretpassword@localhost:5432/$(whoami)\"
-db-schemas = \"api\"
-db-anon-role = \"todo_user\"" > data/db.conf
-
-# Load DB dump
-psql < db.sql
