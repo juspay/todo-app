73d4f3d6af2b640f5375e38cf57f6ca4ae6361d5
shivaraj-bh <me@shivaraj-bh.in>
2024-10-13 23:55:04 +0530

    remove commented create-db script

--- nix/todo-app.nix
+++ nix/todo-app.nix
@@ -11,20 +11,4 @@
   packages = {
     default = self'.packages.todo-app;
   };
-
-
-  # scripts."createdb" = {
-  #   packages = [ pkgs.postgresql ];
-  #   text =
-  #     ''
-  #       # Create a database of your current user
-  #       if ! psql -h "$PWD"/data -lqt | cut -d \| -f 1 | grep -qw "$(whoami)"; then
-  #         createdb -h "$PWD"/data "$(whoami)"
-  #       fi
-
-  #       # Load DB dump
-  #       # TODO: check if schema already exists
-  #       psql -h "$PWD"/data < db.sql
-  #     '';
-  # };
 }
